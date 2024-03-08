b8c78ded0fe48fd7cf017fc8bf65e8a0c8e01eca
Alessandro Beninati <alessandro.beninati@t4v.it>
2024-03-08 17:55:05 +0100

    Added financial data for drill down demo

--- FinancialDemo/GetData.py
+++ FinancialDemo/GetData.py
@@ -19,6 +19,20 @@ data = pd.DataFrame({
     'Financial_Year': np.random.choice(years, num_entries),
     'Amount': np.random.uniform(10000, 100000, num_entries).round(2)  # random financial amounts
 })
+# Generate random dates within a range
+def random_dates(start, end, n=10):
+    start_u = start.value//10**9
+    end_u = end.value//10**9
+
+    return pd.to_datetime(np.random.randint(start_u, end_u, n), unit='s')
+
+start = pd.to_datetime('2018-01-01')
+end = pd.to_datetime('2022-12-31')
+
+# Replace 'Financial_Year' with 'Date'
+data['Date'] = random_dates(start, end, num_entries)
+data.drop('Financial_Year', axis=1, inplace=True)
+
 
 # Save dataframe to a CSV file
 csv_file_path = r'C:\Users\AlessandroBeninati\Python\FinancialDemo\financial_data.csv'
